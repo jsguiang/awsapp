63657a9ac68bdd63044e034162ce9bc5b738d717
jsguiang <jllsabado@gmail.com>
2020-03-25 10:30:54 +0800

    Updates

---
+++ precache-manifest.e047675877e51a5e0b2d6b3b6e58e66b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "6441e99912fff3d69c202e8120fff7c7",
+    "url": "/awsapp/static/media/MyriadPro-Regular.6441e999.woff"
+  },
+  {
+    "revision": "60e3e11856b4c295fec9",
+    "url": "/awsapp/static/css/main.91af1807.chunk.css"
+  },
+  {
+    "revision": "339e3545097a83bbb413",
+    "url": "/awsapp/static/js/runtime~main.339e3545.js"
+  },
+  {
+    "revision": "4dc7bce729757da64114c9a6717d8bbc",
+    "url": "/awsapp/static/media/MyriadPro-Bold.4dc7bce7.svg"
+  },
+  {
+    "revision": "b56ca58f6a12a6994aff",
+    "url": "/awsapp/static/js/2.b56ca58f.chunk.js"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/awsapp/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/awsapp/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/awsapp/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/awsapp/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "e4eb8bf88a2c013871b35c00d4e06b53",
+    "url": "/awsapp/static/media/Client.e4eb8bf8.JPG"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/awsapp/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "648e8f8de0ca96afbcc1f3d0da06e168",
+    "url": "/awsapp/static/media/BGText.648e8f8d.jpg"
+  },
+  {
+    "revision": "78a1f097bee52b9df2fea31165caed6b",
+    "url": "/awsapp/static/media/PSBmini.78a1f097.JPG"
+  },
+  {
+    "revision": "60e3e11856b4c295fec9",
+    "url": "/awsapp/static/js/main.60e3e118.chunk.js"
+  },
+  {
+    "revision": "5d5931716e5966aa3cdd85d73b9deb7e",
+    "url": "/awsapp/static/media/MyriadPro-It.5d593171.woff"
+  },
+  {
+    "revision": "2e17ae6df62d80c50c1682152991cad1",
+    "url": "/awsapp/static/media/MyriadPro-Bold.2e17ae6d.woff"
+  },
+  {
+    "revision": "104190e92f562d06530295f75c2b1df7",
+    "url": "/awsapp/static/media/MyriadPro-Regular.104190e9.eot"
+  },
+  {
+    "revision": "e5515e90e5dbb51353baa5529937b208",
+    "url": "/awsapp/static/media/MyriadPro-Regular.e5515e90.ttf"
+  },
+  {
+    "revision": "3f4d7321c1eb58331ed6d70af9b81176",
+    "url": "/awsapp/static/media/MyriadPro-It.3f4d7321.eot"
+  },
+  {
+    "revision": "e3b26fb46b3a06498ef068469b9433ba",
+    "url": "/awsapp/static/media/MyriadPro-It.e3b26fb4.ttf"
+  },
+  {
+    "revision": "dc897f058c818bfd0be7734e1c142b04",
+    "url": "/awsapp/static/media/MyriadPro-Bold.dc897f05.eot"
+  },
+  {
+    "revision": "ec64422dd057a3e97f118b22c2ac92d5",
+    "url": "/awsapp/static/media/MyriadPro-Bold.ec64422d.ttf"
+  },
+  {
+    "revision": "a3bbee1754800ec1061fe8de38fdce93",
+    "url": "/awsapp/static/media/MyriadPro-Regular.a3bbee17.svg"
+  },
+  {
+    "revision": "4d90006cbe55d6d54beaf0e70ad2224b",
+    "url": "/awsapp/static/media/MyriadPro-It.4d90006c.svg"
+  },
+  {
+    "revision": "b56ca58f6a12a6994aff",
+    "url": "/awsapp/static/css/2.7657a942.chunk.css"
+  },
+  {
+    "revision": "4007e4f39bfa696505a2deb1f9cac2ab",
+    "url": "/awsapp/index.html"
+  }
+];
